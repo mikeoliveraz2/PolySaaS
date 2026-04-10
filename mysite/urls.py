afeda2daa156feb2ab6346c372e3ef73e13f04ff
Michael Oliver <michael.oliver@polysaas.online>
2026-04-11 06:07:46 +0800

    Remove legacy Nextcloud admin view path

--- mysite/urls.py
+++ mysite/urls.py
@@ -53,7 +53,6 @@ urlpatterns = [
     path('admin/select-theme/api/', __import__('dose.admin_views').admin_views.select_theme_api, name='select_theme_api'),
     path('admin/set-theme/', __import__('dose.admin_views').admin_views.set_theme, name='set_theme'),
     path('admin/font-controls/', __import__('dose.admin_views').admin_views.font_controls, name='font_controls'),
-    path('admin/nextcloud-view/', __import__('dose.admin_views').admin_views.nextcloud_view, name='nextcloud_view'),
     path('admin/monitor-logger-view/', __import__('dose.admin_views').admin_views.monitor_logger_view, name='monitor_logger_view'),
     path('admin/odoo-view/', __import__('dose.admin_views').admin_views.odoo_view, name='odoo_view'),
     path('admin/dolibarr-view/', __import__('dose.admin_views').admin_views.dolibarr_view, name='dolibarr_view'),
